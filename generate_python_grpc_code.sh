a5a4868e0abe49154910b25e04c58c93187bf476
Carlo Fragni <carlo@cartesi.io>
2019-05-03 21:42:57 -0300

    Explicit use of python3 to generate python code

--- generate_python_grpc_code.sh
+++ generate_python_grpc_code.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 GRPC_PY_DIR=py
-python -m grpc_tools.protoc  -I. --python_out=$GRPC_PY_DIR --grpc_python_out=$GRPC_PY_DIR core.proto
-python -m grpc_tools.protoc  -I. --python_out=$GRPC_PY_DIR cartesi-base.proto
-python -m grpc_tools.protoc  -I. --python_out=$GRPC_PY_DIR --grpc_python_out=$GRPC_PY_DIR manager-low.proto
-python -m grpc_tools.protoc  -I. --python_out=$GRPC_PY_DIR --grpc_python_out=$GRPC_PY_DIR manager-high.proto
+python3 -m grpc_tools.protoc  -I. --python_out=$GRPC_PY_DIR --grpc_python_out=$GRPC_PY_DIR core.proto
+python3 -m grpc_tools.protoc  -I. --python_out=$GRPC_PY_DIR cartesi-base.proto
+python3 -m grpc_tools.protoc  -I. --python_out=$GRPC_PY_DIR --grpc_python_out=$GRPC_PY_DIR manager-low.proto
+python3 -m grpc_tools.protoc  -I. --python_out=$GRPC_PY_DIR --grpc_python_out=$GRPC_PY_DIR manager-high.proto
 
 
 
