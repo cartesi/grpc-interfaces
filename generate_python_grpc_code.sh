6846394f69710394f63f187bc8538f5ee0a1e61b
Stephen <stephen@cartesi.io>
2019-09-22 21:58:52 +0800

    Add logger to py generate script

--- generate_python_grpc_code.sh
+++ generate_python_grpc_code.sh
@@ -5,6 +5,7 @@ python3 -m grpc_tools.protoc  -I. --python_out=$GRPC_PY_DIR --grpc_python_out=$G
 python3 -m grpc_tools.protoc  -I. --python_out=$GRPC_PY_DIR cartesi-base.proto
 python3 -m grpc_tools.protoc  -I. --python_out=$GRPC_PY_DIR --grpc_python_out=$GRPC_PY_DIR manager-low.proto
 python3 -m grpc_tools.protoc  -I. --python_out=$GRPC_PY_DIR --grpc_python_out=$GRPC_PY_DIR manager-high.proto
+python3 -m grpc_tools.protoc  -I. --python_out=$GRPC_PY_DIR --grpc_python_out=$GRPC_PY_DIR logger-high.proto
 
 
 
