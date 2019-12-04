0eb340563dc88dc1917ffdb25a0379843737ed1a
Victor Fusco <victor@cartesi.io>
2019-12-04 01:01:02 -0300

    Update rust libs

--- emulator_interface/build.rs
+++ emulator_interface/build.rs
@@ -11,17 +11,22 @@
 
 extern crate protoc_rust_grpc;
 
+use std::path::Path;
+
 fn main() {
-    //match
-    protoc_rust_grpc::run(protoc_rust_grpc::Args {
-        out_dir: "src",
-        includes: &["../../"],
-        input: &[
-            "../../cartesi-base.proto",
-            "../../manager-high.proto",
-        ],
-        rust_protobuf: true, // generate protobuf messages, not just services
-        ..Default::default()
-    })
-    .expect("protoc-rust-grpc");
+    let generated_files = [
+        "src/cartesi_base.rs",
+        "src/manager_high.rs",
+        "src/manager_high_grpc.rs",
+    ];
+    if !generated_files.iter().all(|&f| Path::new(f).exists()) {
+        protoc_rust_grpc::run(protoc_rust_grpc::Args {
+            out_dir: "src",
+            includes: &["../"],
+            input: &["../cartesi-base.proto", "../manager-high.proto"],
+            rust_protobuf: true, // generate protobuf messages, not just services
+            ..Default::default()
+        })
+        .expect("protoc-rust-grpc");
+    }
 }
